73557660da018ace35dfc8178a68aaa05e87ec5a
chung <tranducchung1st@gmail.com>
2019-05-29 10:16:09 +0700

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RegisterFinalComponent } from './register-final/register-final.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    TemplateDrivenFormComponent,
+    RegisterFinalComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.scss']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+  profile = {
+    username: 'Bob',
+    email: 'abc@deg.com',
+    facebook: 'facebook.com',
+    twitter: 'twitter.com',
+    website: 'example.com',
+    tel: '1234-5678-90'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSubmit(profileForm) {
+    console.log(profileForm);
+  }
+}
